98e3fb22732e484a8152fa3c81e9d1f817acc10e
kelerjiman <sayed.ali.hosseini.1988@gmail.com>
2019-07-10 22:37:39 +0430

    Fix : AI Handling

--- Assets/Scripts/RotationHandle.cs
+++ Assets/Scripts/RotationHandle.cs
@@ -5,32 +5,72 @@ using UnityEngine;
 public class RotationHandle : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] float Angle = 30;
-    NormCars NormalCars;
-    Quaternion NewRot;
     [SerializeField]
-    [Range(0, 1)]
-    float H_R_Speed = 0.5f;
+    float Angle = 6, Input_setY = 0
+        , maxAngle, minAngle;
+    [SerializeField]
+    float H_R_Speed = 0;
+    NormCars NormalCars;
+    cars InverseCar;
+    border GBorder;
+    Quaternion DefRot, NewRot;
     void Start()
     {
         NewRot = transform.rotation;
-        NormalCars = transform.parent.GetComponent<NormCars>();
+        DefRot = NewRot;
+        H_R_Speed = GameManager.RotateSpeed;
+        GBorder = FindObjectOfType<border>();
+        if (transform.parent.parent.GetComponent<Spawner>().reverseLine)
+        {
+            InverseCar = transform.parent.GetComponent<cars>();
+        }
+        else
+            NormalCars = transform.parent.GetComponent<NormCars>();
+
+        maxAngle = transform.rotation.eulerAngles.y + 6;
+        minAngle = transform.rotation.eulerAngles.y - 6;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (NormalCars.TimeToTurn)
+        H_R_Speed = GameManager.RotateSpeed;
+        Direction();
+        Handling();
+    }
+    void Direction()
+    {
+        if (InverseCar.TimeToTurn)
         {
-            NewRot = transform.rotation;
-            if (NormalCars.GoRight)
-                NewRot = Quaternion.AngleAxis(NewRot.eulerAngles.y + 1, Vector3.up);
-            else
-            if (NormalCars.GoLeft)
-                NewRot = Quaternion.AngleAxis(NewRot.eulerAngles.y - 1, Vector3.up);
+            if (InverseCar.GoRight)
+            {
+                Input_setY = +1;
+            }
+            if (InverseCar.GoLeft)
+            {
+                Input_setY = -1;
+            }
+        }
+        if (!InverseCar.TimeToTurn || InverseCar.NoWay || !InverseCar.KnowDir)
+        {
+            Input_setY = 0;
+        }
+    }
+    void Handling()
+    {
+        if (transform.eulerAngles.y < maxAngle && Input_setY == 1)
+            NewRot = Quaternion.Euler(0, transform.rotation.eulerAngles.y + Input_setY, 0);
+        if (transform.eulerAngles.y > minAngle && Input_setY == -1)
+            NewRot = Quaternion.Euler(0, transform.rotation.eulerAngles.y + Input_setY, 0);
+        if (Input_setY != 0)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, NewRot, H_R_Speed );
+            print(transform.rotation.eulerAngles.y);
         }
         else
-            NewRot = Quaternion.Lerp(NewRot, Quaternion.identity, GameManager.RotateSpeed);
-        transform.rotation = Quaternion.Lerp(transform.rotation, NewRot, GameManager.RotateSpeed);
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, DefRot, H_R_Speed );
+
+        }
     }
 }
