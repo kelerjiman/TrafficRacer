813d4b772c94f18a88099c4f692311cb64cf99ee
kelerjiman <sayed.ali.hosseini.1988@gmail.com>
2019-07-10 23:08:14 +0430

    Fix : Delete unnecessary Code
    delete rig

--- Assets/Scripts/cars.cs
+++ Assets/Scripts/cars.cs
@@ -1,115 +1,215 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-[RequireComponent(typeof(Rigidbody))]
 public class cars : MonoBehaviour
 {// Start is called before the first frame update
-    public float Zspeed = -2f, DefaultSpeed;
+    public float Zspeed = -2f, DefaultSpeed, ChangeLineSpeed;
     [SerializeField]
     [Range(0, 1)]
     float H_R_Speed = 1f;
-    Quaternion defRot;
+    Quaternion NewRot;
+    [SerializeField] Transform RotationChild;
     public bool Inverse = false;
-    border GBorder;
-    Rigidbody rig;
-    [SerializeField] bool TimeToTurn = false, stop = false;
+    public bool TimeToTurn = false, NoWay = false, GoLeft = false, GoRight = false, KnowDir = false;
     [SerializeField] LightManager Detector;
     [Header("will delete after complete")]
     [SerializeField]
     GameObject front, back;
-    [SerializeField]
-    float angle = 30f;
-
-    private void OnCollisionEnter(Collision collision)
+    private void Start()
     {
-        if (collision.gameObject.tag != "ground" && collision.gameObject.tag != "Player"
-            && collision.gameObject.layer!=LayerMask.NameToLayer("Cars"))
-            Destroy(gameObject);
-    }
-    private void Awake()
-    {
-        H_R_Speed = 1f;
-        angle = 30f;
+        gameObject.tag = "Obs";
         gameObject.layer = LayerMask.NameToLayer("Cars");
-    }
-    void Start()
-    {
-        tag = "Obs";
-        if (gameObject.GetComponents<BoxCollider>().Length == 0)
-            gameObject.AddComponent<BoxCollider>().enabled = true;
-        if (gameObject.GetComponents<MeshCollider>().Length > 0)
-            Destroy(GetComponent<MeshCollider>());
-        rig = GetComponent<Rigidbody>();
-        rig.useGravity = true;
-        GBorder = FindObjectOfType<border>();
+        RotationChild = transform.Find("RotationChild");
+        NewRot = transform.rotation;
+        ChangeLineSpeed = GameManager.ChangeLineSpeed;
         DefaultSpeed = Zspeed;
         Detector = transform.Find("Detector").GetComponent<LightManager>();
-        defRot = transform.rotation;
-        if (Inverse)
-        {
-            defRot = Quaternion.LookRotation(GBorder.transform.position);
-        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        AIMoveBehavior();
-        AIChooseDirection();
-    }
-
-    void AIMoveBehavior()
-    {
-        if (Detector.front.DirClosed == true)
+        if (Detector.front.DirClosed)
             TimeToTurn = true;
         else
             TimeToTurn = false;
+
+        LetsMove();
+        AIMoveBehavior();
+        AIChooseDirection();
+        ChangeDirection();
+    }
+    void AIMoveBehavior()
+    {
+        if (Detector.front.DirClosed && Detector.FrontObject != null)
+        {
+            if (Detector.FrontObject.layer == LayerMask.NameToLayer("Cars"))
+                Zspeed = Detector.FrontObject.GetComponent<cars>().Zspeed;
+        }
+        else
+            Zspeed = DefaultSpeed;
     }
     void AIChooseDirection()
     {
-        //badan rooye An kar shavad
-        if (TimeToTurn)
+        if (!TimeToTurn)
         {
-            front = Detector.FrontObject;
-            if (front.gameObject != null && front.gameObject.activeInHierarchy)
+            KnowDir = false;
+            GoRight = false;
+            GoLeft = false;
+            return;
+        }
+        if (!KnowDir)
+        {
+            if (!Detector.left.DirClosed || !Detector.right.DirClosed)
             {
-                if (front.layer == LayerMask.NameToLayer("Cars"))
-                    Zspeed = front.GetComponent<cars>().Zspeed;
-            }
-            if (Inverse)
-            {
-                if (!Detector.left.DirClosed || !Detector.right.DirClosed)
-                {
-                    var temp = transform.rotation;
-                    if (!Detector.left.DirClosed)
-                    {
-                        temp = Quaternion.Euler(0, transform.rotation.eulerAngles.y - angle * Time.deltaTime, 0);
-                    }
-                    else
-                    if (!Detector.right.DirClosed)
-                    {
-                        temp = Quaternion.Euler(0, transform.rotation.eulerAngles.y + angle * Time.deltaTime, 0);
 
-                    }
-                    transform.rotation = Quaternion.Lerp(transform.rotation, temp, H_R_Speed);
+                NoWay = false;
+                if (!Detector.left.DirClosed)
+                {
+                    KnowDir = true;
+                    GoLeft = true;
+                    GoRight = false;
+
+                }
+                else
+                {
+                    KnowDir = true;
+                    GoRight = true;
+                    GoLeft = false;
+
                 }
             }
             else
             {
-
+                GoLeft = false;
+                GoRight = false;
+                NoWay = true;
+                KnowDir = false;
             }
         }
-        else
+
+    }
+    void ChangeDirection()
+    {
+        if (!NoWay)
         {
-            Zspeed = DefaultSpeed;
-            LetsMove();
+            if (GoLeft && !Detector.left.DirClosed)
+                transform.position -= Vector3.left * Time.deltaTime * ChangeLineSpeed;
+            else
+            if (GoRight && !Detector.right.DirClosed)
+                transform.position -= Vector3.right * Time.deltaTime * ChangeLineSpeed;
+            else
+                KnowDir = false;
         }
     }
+
     void LetsMove()
     {
         transform.Translate(0, 0, Zspeed * Time.deltaTime);
-        //if (Inverse)
-        transform.rotation = Quaternion.Lerp(transform.rotation, defRot, H_R_Speed * Time.deltaTime);
     }
 }
+#region OldCode
+
+//public float Zspeed = -2f, DefaultSpeed, ChangeLineSpeed;
+//[SerializeField]
+//[Range(0, 1)]
+//float H_R_Speed = 1f;
+//Quaternion NewRot;
+//[SerializeField] Transform RotationChild;
+//public bool Inverse = false;
+//Rigidbody rig;
+//public bool TimeToTurn = false, NoWay = false, GoLeft = false, GoRight = false, KnowDir = false;
+//[SerializeField] LightManager Detector;
+//[Header("will delete after complete")]
+//[SerializeField]
+//GameObject front, back;
+//private void Start()
+//{
+//    gameObject.layer = LayerMask.NameToLayer("Cars");
+//    RotationChild = transform.Find("RotationChild");
+//    NewRot = transform.rotation;
+//    ChangeLineSpeed = GameManager.ChangeLineSpeed;
+//    DefaultSpeed = Zspeed;
+//    Detector = transform.Find("Detector").GetComponent<LightManager>();
+//}
+
+//private void Update()
+//{
+//    if (Detector.front.DirClosed)
+//        TimeToTurn = true;
+//    else
+//        TimeToTurn = false;
+
+//    LetsMove();
+//    AIMoveBehavior();
+//    AIChooseDirection();
+//    ChangeDirection();
+//}
+//void AIMoveBehavior()
+//{
+//    if (Detector.front.DirClosed && Detector.FrontObject != null)
+//    {
+//        if (Detector.FrontObject.layer == LayerMask.NameToLayer("Cars"))
+//            Zspeed = Detector.FrontObject.GetComponent<cars>().Zspeed;
+//    }
+//    else
+//        Zspeed = DefaultSpeed;
+//}
+//void AIChooseDirection()
+//{
+//    if (!TimeToTurn)
+//    {
+//        KnowDir = false;
+//        GoRight = false;
+//        GoLeft = false;
+//        return;
+//    }
+//    if (!KnowDir)
+//    {
+//        if (!Detector.left.DirClosed || !Detector.right.DirClosed)
+//        {
+
+//            NoWay = false;
+//            if (!Detector.left.DirClosed)
+//            {
+//                KnowDir = true;
+//                GoLeft = true;
+//                GoRight = false;
+
+//            }
+//            else
+//            {
+//                KnowDir = true;
+//                GoRight = true;
+//                GoLeft = false;
+
+//            }
+//        }
+//        else
+//        {
+//            GoLeft = false;
+//            GoRight = false;
+//            NoWay = true;
+//            KnowDir = false;
+//        }
+//    }
+
+//}
+//void ChangeDirection()
+//{
+//    if (!NoWay)
+//    {
+//        if (GoLeft && !Detector.left.DirClosed)
+//            transform.position -= Vector3.left * Time.deltaTime * ChangeLineSpeed;
+//        else
+//        if (GoRight && !Detector.right.DirClosed)
+//            transform.position -= Vector3.right * Time.deltaTime * ChangeLineSpeed;
+//        else
+//            KnowDir = false;
+//    }
+//}
+
+//void LetsMove()
+//{
+//    transform.Translate(0, 0, Zspeed * Time.deltaTime);
+//}
+#endregion
