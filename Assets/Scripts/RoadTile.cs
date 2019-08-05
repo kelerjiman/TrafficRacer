a755a63752470caa1465ad2cdf5e5468c97673c6
kelerjiman <sayed.ali.hosseini.1988@gmail.com>
2019-08-05 23:08:56 +0430

    Add : RoadTile Script
    for moving and tile behaviors

---
+++ Assets/Scripts/RoadTile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadTile : MonoBehaviour
+{
+    [SerializeField] float speed = 1;
+    private void Update()
+    {
+        speed = GameManager.GlobalSpeed;
+        MovingBehavior();
+    }
+    void MovingBehavior()
+    {
+        var temp = transform.position;
+        temp.z -= 1;
+        transform.position = Vector3.Lerp(transform.position, temp, speed * Time.deltaTime);
+    }
+}
