6b77295d68ca03a708f65f5a1d5295f8cf02e85d
kelerjiman <sayed.ali.hosseini.1988@gmail.com>
2020-03-14 20:46:36 +0330

    Create PurchaseCarWindow.cs

---
+++ Assets/Scripts/PurchaseCarWindow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PurchaseCarWindow : GenericWindow
+{
+    [SerializeField] GameObject alarmText;
+    public CardButton cardButton;
+    [SerializeField] Text Price_text;
+    public override void Start()
+    {
+        base.Start();
+    }
+    public override void Update()
+    {
+        base.Update();
+        if (!cardButton.Is_Locked)
+            gameObject.SetActive(false);
+    }
+    public void SetInfo(int price)
+    {
+        alarmText.SetActive(false);
+        Price_text.text = price.ToString();
+    }
+    public void purchasing()
+    {
+        if (cardButton.Is_Locked)
+        {
+            bool temp = GameManager.Instance.purchaseItem(cardButton.price);
+            cardButton.Is_Locked = !temp;
+            cardButton.saveData();
+            if (temp)
+                OnNext(PreviousWindow);
+            alarmText.SetActive(cardButton.Is_Locked);
+        }
+    }
+
+}
