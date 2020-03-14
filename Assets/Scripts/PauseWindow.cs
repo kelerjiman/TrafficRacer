58ad7f59d8b25fc4d5fcff271bd6217991608597
kelerjiman <sayed.ali.hosseini.1988@gmail.com>
2020-03-14 20:46:27 +0330

    Update PauseWindow.cs

--- Assets/Scripts/PauseWindow.cs
+++ Assets/Scripts/PauseWindow.cs
@@ -11,6 +11,12 @@ public class PauseWindow : GenericWindow
         base.Start();
 
     }
+    public override void Update()
+    {
+        base.Update();
+        if (!PauseButton.gameObject.activeSelf)
+            PauseButton.gameObject.SetActive(false);
+    }
     public override void OnCloseButton()
     {
         SceneManager.LoadScene(0);
@@ -18,5 +24,12 @@ public class PauseWindow : GenericWindow
     public override void OnNextScene(int SIndex)
     {
         SceneManager.LoadScene(SIndex);
+    }
+    public override void OnPauseButton()
+    {
+        PreviousWindow.SetActive(true);
+        PreviousWindow.GetComponent<GenericWindow>().Start();
+        Time.timeScale = 1;
+        gameObject.SetActive(false);
     }
 }
