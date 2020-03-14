0e631e290fbdc6a538f362080d9fb22f3fa04226
kelerjiman <sayed.ali.hosseini.1988@gmail.com>
2020-03-14 20:46:32 +0330

    Update OptionWindow.cs

--- Assets/Scripts/OptionWindow.cs
+++ Assets/Scripts/OptionWindow.cs
@@ -5,10 +5,35 @@ using UnityEngine.UI;
 
 public class OptionWindow : GenericWindow
 {
-      public override void Start()
+    [SerializeField] Slider masterVolume;
+    [SerializeField] Toggle mute;
+    private int m_KindOfControl = 0;
+    public override void Start()
     {
         base.Start();
+        OnLoadData();
         Button btn = CloseButton.GetComponent<Button>();
         CloseButton.onClick.AddListener(OnCloseButton);
+
+    }
+    public override void OnCloseButton()
+    {
+        base.OnCloseButton();
+        OnSaveButton();
+    }
+    public void OnLoadData()
+    {
+        masterVolume.value = PlayerPrefsScript.getMastervolume();
+        mute.isOn = PlayerPrefsScript.getMutevolume();
+    }
+    public void OnSaveButton()
+    {
+        PlayerPrefsScript.setMastervolume(masterVolume.value);
+        PlayerPrefsScript.setMutevolume(mute.isOn);
+        PlayerPrefsScript.setKindOfControl(m_KindOfControl);
+    }
+    public void OnSelectedControl(int index)
+    {
+        m_KindOfControl = index;
     }
 }
