d94138dbc562162d86ff4368f62579ed26de12f0
kelerjiman <sayed.ali.hosseini.1988@gmail.com>
2020-03-14 20:46:15 +0330

    Create GenericSpawner.cs

---
+++ Assets/Scripts/Pool_/GenericSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericSpawner : MonoBehaviour
+{
+    public Vector2 randomTimeRange = Vector2.zero;
+    [SerializeField]
+    protected string Tag = string.Empty;
+    [SerializeField]
+    protected GameObject respawnPoint = null;
+    protected float Timer = 0;
+    protected float m_Timer = 0f;
+    [SerializeField] protected ObjectPooler OP;
+
+    public virtual void Start()
+    {
+        
+    }
+    public virtual void Update()
+    {
+        
+    }
+    public virtual void SpawnObjects()
+    {
+        OP.SpawnFromPool(Tag, respawnPoint.transform.position, respawnPoint.transform.rotation);
+    }
+}
