fe7ee6e8c003c879d4af367b2c10ef5cdc57c087
kelerjiman <sayed.ali.hosseini.1988@gmail.com>
2020-03-14 20:46:05 +0330

    Create ObjectSpawner.cs

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : GenericSpawner
+{
+    public override void Start()
+    {
+        base.Start();
+        Timer = Random.Range(randomTimeRange.x, randomTimeRange.y);
+    }
+    public override void Update()
+    {
+        base.Update();
+        m_Timer += Time.deltaTime;
+        if (m_Timer >= Timer)
+        {
+            SpawnObjects();
+            m_Timer = 0;
+            Timer = Random.Range(randomTimeRange.x, randomTimeRange.y);
+        }
+    }
+}
