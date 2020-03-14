22ca6bfcce72a8fa4dac9b83e5d068ef83d33570
kelerjiman <sayed.ali.hosseini.1988@gmail.com>
2020-03-14 20:49:16 +0330

    Update Spawner.cs

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -38,7 +38,7 @@ public class Spawner : MonoBehaviour
     }
     private void Update()
     {
-        if (GameManager.GM.GM_Is_Accident)
+        if (GameManager.Instance.GM_Is_Accident)
             return;
         m_NumberOfChildren = gameObject.GetComponentsInChildren<cars>().Length;
         if (m_NumberOfChildren < VehicleCount)
@@ -103,48 +103,5 @@ public class Spawner : MonoBehaviour
     {
         m_currentOBJ = PrefabList[UnityEngine.Random.Range(0, PrefabList.Count)];
     }
-    #region old code
-
-    //// Start is called before the first frame update
-    //public float GlobalSpeed = 30f;
-    //public bool doJob = false;
-    //[SerializeField] List<GameObject> PrefabList;
-    //[SerializeField] int counter;
-    //[Header("respawn time (x=min , y= max")]
-    //[Tooltip("Edit the X to Zero and  Y for random respawn")]
-    //[SerializeField]
-    //Vector2 RespawnTimer = new Vector2(1f, 5f);
-    //public bool reverseLine = false;
-    //void Start()
-    //{
-    //    StartCoroutine(RewpawnDelay());
-    //}
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    GlobalSpeed = GameManager.GlobalSpeed;
-    //}
-    //IEnumerator RewpawnDelay()
-    //{
-    //    yield return new WaitForSeconds(Random.Range(RespawnTimer.x, RespawnTimer.y));
-    //    if (doJob)
-    //    {
-    //        counter = Random.Range(0, PrefabList.Count);
-    //        var x = Instantiate(PrefabList[counter], transform.position, Quaternion.identity, transform);
-    //        if (reverseLine)
-    //        {
-    //            Destroy(x.GetComponent<NormCars>());
-    //            x.transform.rotation = Quaternion.Inverse(transform.rotation);
-    //            x.GetComponent<cars>().MainSpeed = GlobalSpeed * 2 * Random.Range(0.5f, 1);
-    //        }
-    //        else
-    //        {
-    //            Destroy(x.GetComponent<cars>());
-    //            x.GetComponent<NormCars>().MainSpeed = -GlobalSpeed * Random.Range(0.5f, 1);
-    //        }
-    //    }
-    //    StartCoroutine(RewpawnDelay());
-    //}
-    #endregion
+    
 }
