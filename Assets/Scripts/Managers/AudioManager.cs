ded5eaa8e4024b2c61283bdb5a877515a7f1f0d4
kelerjiman <sayed.ali.hosseini.1988@gmail.com>
2020-03-21 23:13:30 +0430

    Create AudioManager.cs

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public SoundProperties[] sounds;
+    public static AudioManager Instance;
+    public AudioMixer MasteMixer;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        MasteMixer.SetFloat("Background", PlayerPrefsScript.getMastervolume());
+        var temp = GetComponent<AudioSource>();
+        SetData(ref temp, "Theme");
+        temp.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void SetData(ref AudioSource source, string _name)
+    {
+        foreach (var s in sounds)
+        {
+            if (_name == s.Name)
+            {
+                s.GetSource(source);
+                break;
+            }
+        }
+    }
+}
