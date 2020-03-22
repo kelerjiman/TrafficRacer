8919e9344ad17934e8db6d942e35a6a83f321d5e
kelerjiman <sayed.ali.hosseini.1988@gmail.com>
2020-03-22 18:04:36 +0430

    Create SO_CarCard.cs

---
+++ Assets/Scripts/ScriptableObjects/CardInfo/SO_CarCard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName ="Car_Card_Prop",menuName ="Car Card")]
+public class SO_CarCard : ScriptableObject
+{
+    [SerializeField] Sprite CarIcon;
+    [SerializeField] string CarName;
+    [SerializeField] GameObject Prefab;
+    [Range(0,1)]
+    [SerializeField] float ProductCoinRate = 1;
+    [Range(0,1)]
+    [SerializeField] float MultipleScoreRate = 1;
+    [SerializeField] int Price = 0;
+    [SerializeField] SoundProperties[] sound;
+    public Sprite GetCarIcon()
+    {
+
+        return CarIcon;
+    }
+    public string GetCarName()
+    {
+        return CarName;
+    }
+    public float GetCoinRate()
+    {
+        return ProductCoinRate;
+    }
+    public float GetScoreRate()
+    {
+        return MultipleScoreRate;
+    }
+    public GameObject GetPrefab()
+    {
+        return Prefab;
+    }
+    public int GetPrice()
+    {
+        return Price;
+    }
+}
