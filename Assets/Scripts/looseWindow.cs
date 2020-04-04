018c930476662560da0c3dfeda7c4ad0648873c4
kelerjiman <sayed.ali.hosseini.1988@gmail.com>
2020-04-04 18:47:54 +0430

    Update looseWindow.cs

--- Assets/Scripts/looseWindow.cs
+++ Assets/Scripts/looseWindow.cs
@@ -1,11 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class looseWindow : GenericWindow
 {
+    [Space(10)]
+    [Header("===========================")]
+    [SerializeField]
+    Text m_TotalDistance;
+    [SerializeField]
+    Text m_OverTakes;
+    [SerializeField]
+    Text m_Opposite;
+    [SerializeField]
+    Text m_PowerUps;
+    [SerializeField]
+    Text m_TotalScore;
+    ScoreManager SM;
     public override void Start()
     {
+        SM = FindObjectOfType<ScoreManager>();
         base.Start();
         CloseButton.gameObject.SetActive(false);
     }
@@ -16,8 +31,12 @@ public class looseWindow : GenericWindow
     }
     public override void reloadSetting()
     {
+        m_TotalDistance.text = SM.TotalDistance.ToString();
+        m_TotalScore.text= SM.TotalScore.ToString();
+        m_OverTakes.text = SM.OverTakes.ToString();
+        m_PowerUps.text = SM.Powerups.ToString();
+        m_Opposite.text = SM.OppositeLine.ToString();
         GameManager.Instance.player.gameObject.SetActive(false);
         base.reloadSetting();
-
     }
 }
