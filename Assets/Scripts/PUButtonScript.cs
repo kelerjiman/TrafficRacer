e625842b46729c9b8a9a99126a482637aefd37e9
kelerjiman <sayed.ali.hosseini.1988@gmail.com>
2020-04-06 05:45:02 +0430

    Create : PUButtonScript.cs and Update those prefabs

---
+++ Assets/Scripts/PUButtonScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PUButtonScript : MonoBehaviour
+{
+    [SerializeField] string Name;
+    [SerializeField] Text PriceText;
+    private int currentPrice = 300;
+    [SerializeField] bool MaxUpgrade = false;
+    private void Start()
+    {
+        if (MaxUpgrade)
+        {
+            PriceText.text = "MAX";
+            GetComponent<Button>().interactable = false;
+        }
+        else
+            PriceText.text = currentPrice.ToString();
+    }
+    public void OnButtonClick()
+    {
+        Debug.Log("Upgradeof " + name + " was completed !");
+        currentPrice += 300;
+        PriceText.text = (currentPrice).ToString();
+    }
+}
